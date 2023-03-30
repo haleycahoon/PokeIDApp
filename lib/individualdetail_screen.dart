bf2a6de650e6852f4b7288784d858b0bd86e41b7
Haley Cahoon <haleyac55@gmail.com>
2023-03-30 16:58:54 -0400

    adding comments for individual pokemon

--- lib/individualdetail_screen.dart
+++ lib/individualdetail_screen.dart
@@ -1,1 +1,9 @@
 // this file is to display the individual pokemon that the user selects
+
+// pokemon name
+// pokemon number 
+// pokemon picture 
+// pokemon types 
+// pokemon height
+// pokemon weight
+// pokemon habitat
