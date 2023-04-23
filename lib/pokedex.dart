3d860b4319c1b04ff73bab567adc33c9101ffd95
Edward Nieves <edwardn102@icloud.com>
2023-04-23 18:53:45 -0400

    new pics

---
+++ lib/pokedex.dart
@@ -0,0 +1,105 @@
+import 'dart:convert';
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<List<Pokemon>> fetchPhotos(http.Client client) async {
+  final response = await client.get(Uri.parse(
+      'https://raw.githubusercontent.com/hungps/flutter_pokedex/master/assets/pokemons.json'));
+
+  // Use the compute function to run parsePhotos in a separate isolate.
+  return compute(parsePhotos, response.body);
+}
+
+// A function that converts a response body into a List<Photo>.
+List<Pokemon> parsePhotos(String responseBody) {
+  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+  return parsed.map<Pokemon>((json) => Pokemon.fromJson(json)).toList();
+}
+
+class Pokemon {
+  final String name;
+  final String id;
+  final String imageurl;
+
+  const Pokemon({
+    required this.name,
+    required this.id,
+    required this.imageurl,
+  });
+
+  factory Pokemon.fromJson(Map<String, dynamic> json) {
+    return Pokemon(
+        name: json['name'] as String,
+        id: json['id'] as String,
+        imageurl: json["imageurl"] as String);
+  }
+}
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const appTitle = 'Isolate Demo';
+
+    return const MaterialApp(
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: FutureBuilder<List<Pokemon>>(
+        future: fetchPhotos(http.Client()),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text('An error has occurred!'),
+            );
+          } else if (snapshot.hasData) {
+            return PokemonsList(pokemons: snapshot.data!);
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class PokemonsList extends StatelessWidget {
+  const PokemonsList({super.key, required this.pokemons});
+
+  final List<Pokemon> pokemons;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+      ),
+      itemCount: pokemons.length,
+      itemBuilder: (context, index) {
+        return Image.network(pokemons[index].imageurl);
+      },
+    );
+  }
+}
